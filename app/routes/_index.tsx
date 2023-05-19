af4d9b117cb9f117d0128d8b44ca07b593471fad
branmar97 <brandonmar97@gmail.com>
2023-05-18 22:32:42 -0400

    storefront query

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,32 @@
+import { useLoaderData } from "@remix-run/react";
+import type { LoaderArgs, LoaderFunction } from "@shopify/remix-oxygen";
+
+export type LoaderData<T extends LoaderFunction> = Awaited<ReturnType<T>>
+
 export function meta() {
     return [
       { title: 'Frostbite Boards' },
       { description: 'Conquer the chill.' },
     ];
-  }
-  export default function Index() {
-    return (
-      <div>
-        <h3>Welcome to Frostbite Boards!</h3>
-      </div>
-    );
-  }
+}
+
+export const loader = async ({ context }: LoaderArgs ) => { 
+    return await context.storefront.query(COLLECTIONS_QUERY);
+}
+
+export default function Index() {
+    const { collections } = useLoaderData<LoaderData<typeof loader>>();
+    return <p>Frostbite Boards</p>;
+}
+
+const COLLECTIONS_QUERY = `#graphql
+    query FeaturedCollections {
+        collections(first: 3, query: "collection_type:smart") {
+            nodes {
+                id
+                title
+                handle
+            }
+        }
+    }
+`;
