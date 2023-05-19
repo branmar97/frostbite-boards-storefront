ce2dc8cca4b1c74610c01e133979cc83d1ee4b99
branmar97 <brandonmar97@gmail.com>
2023-05-18 22:05:45 -0400

    add index route

---
+++ app/routes/_index.tsx
@@ -0,0 +1,13 @@
+export function meta() {
+    return [
+      { title: 'Frostbite Boards' },
+      { description: 'Conquer the chill.' },
+    ];
+  }
+  export default function Index() {
+    return (
+      <div>
+        <h3>Welcome to Frostbite Boards!</h3>
+      </div>
+    );
+  }
